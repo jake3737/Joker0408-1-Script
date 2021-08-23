645d194048027706441b14893509792296d11620
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-08-24 04:12:44 +0800

    Update jd_bean_change.js

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -149,7 +149,7 @@ ReturnMessage+=`🐶今日过期：${$.expirejingdou}京豆 \n`;
 
         }
     }
-    ReturnMessage+=`###### 红包明细 ######`;
+    ReturnMessage+=`\n###### 红包明细 ######\n`;
     ReturnMessage+=`${$.message}\n\n`;
     allMessage+=ReturnMessage;
     $.msg($.name, '', ReturnMessage , {"open-url": "https://bean.m.jd.com/beanDetail/index.action?resourceValue=bean"});
