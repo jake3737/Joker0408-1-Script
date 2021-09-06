bf48fb20090dcf5344fe6bd0869aebdfa5ad7095
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-09-06 20:18:32 +0800

    Update jd_bean_change.js

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -132,15 +132,16 @@ async function showMsg() {
     if(typeof $.JDtotalcash !== "undefined"){
     ReturnMessage+=`💰极速金币：${$.JDtotalcash}枚(${$.JDtotalcash / 10000}元)\n*************\n`;
     }
-             ReturnMessage+=`👨‍🌾东东农场：${$.JdFarmProdName}\n👨‍🌾农场进度：(${(($.JdtreeEnergy / $.JdtreeTotalEnergy) * 100).toFixed(2)}%)`;
+    if($.JdFarmProdName != ""){
+    ReturnMessage+=`👨‍🌾东东农场：${$.JdFarmProdName}\n👨‍🌾农场进度：(${(($.JdtreeEnergy / $.JdtreeTotalEnergy) * 100).toFixed(2)}%)`;
              if($.JdwaterD!='Infinity' && $.JdwaterD!='-Infinity'){
-                ReturnMessage+=`,${$.JdwaterD === 1 ? '明天' : $.JdwaterD === 2 ? '后天' : $.JdwaterD + '天'}可兑换\n`;
+    ReturnMessage+=`,${$.JdwaterD === 1 ? '明天' : $.JdwaterD === 2 ? '后天' : $.JdwaterD + '天'}可兑换\n`;
             } else {
-                ReturnMessage+=`\n`;
+    ReturnMessage+=`\n`;
             }
         } else {
-            ReturnMessage+=`👨‍🌾东东农场：${$.JdFarmProdName}\n`;
-        }
+    ReturnMessage+=`👨‍🌾东东农场：${$.JdFarmProdName}\n`;
+    }
     }
     const response = await await PetRequest('energyCollect');
     const initPetTownRes = await PetRequest('initPetTown');
@@ -152,7 +153,6 @@ async function showMsg() {
      //ReturnMessage += `已有${response.result.medalNum}块勋章，还需${response.result.needCollectMedalNum}块\n`;
     }
     }
-    if($.JdFarmProdName != ""){
     if ($.jxFactoryInfo) {
     ReturnMessage+= `🏭京喜工厂：${$.jxFactoryInfo}\n`
     }
