1d6b028c2c96e12783d0379d8c282584c46bebf6
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-09-17 03:06:53 +0800

    Update A_OpenCard.py

--- A_OpenCard.py
+++ A_OpenCard.py
@@ -187,7 +187,7 @@ pwd = os.path.dirname(os.path.abspath(__file__)) + os.sep
 
 ######
 ## 账号
-OpenCardUserList = ({5..9})
+OpenCardUserList = (5 6 7 8 9)
 ## 随机Cookie
 OpenCardRandomCK = false
 openCardBean = 10
