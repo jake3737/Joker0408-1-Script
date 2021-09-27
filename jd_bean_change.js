91121b7890821ce4a49580323e55ed543c90b30f
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-09-27 19:43:12 +0800

    Update jd_bean_change.js

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -574,31 +574,31 @@ function redPacket() {
     if ($.jdRed > 0)
     $.message += `🧧京东红包：${$.jdRed}元`;
     if ($.jdRedExpire > 0)
-    $.message += ` || 到期:${$.jdRedExpire.toFixed(2)}元`;
+    $.message += ` || 到期${$.jdRedExpire.toFixed(2)}元`;
     if ($.jdRed > 0)
     $.message += `\n`;
     if ($.jxRed > 0)
     $.message += `🧧京喜红包：${$.jxRed}元`;
     if ($.jxRedExpire > 0)
-    $.message += ` || 到期：${$.jxRedExpire.toFixed(2)}元`;
+    $.message += ` || 到期${$.jxRedExpire.toFixed(2)}元`;
     if ($.jxRed > 0)
     $.message += `\n`;
     if ($.jsRed > 0)
     $.message += `🧧极速红包：${$.jsRed}元`;
     if ($.jsRedExpire > 0)
-    $.message += ` || 到期：${$.jsRedExpire.toFixed(2)}元`;
+    $.message += ` || 到期${$.jsRedExpire.toFixed(2)}元`;
     if ($.jsRed > 0)
     $.message += `\n`;
     if ($.jdhRed > 0)
     $.message += `🧧健康红包：${$.jdhRed}元`;
     if ($.jdhRedExpire > 0)
-    $.message += ` || 到期：${$.jdhRedExpire.toFixed(2)}元`;
+    $.message += ` || 到期${$.jdhRedExpire.toFixed(2)}元`;
     if ($.jdhRed > 0)
     $.message += `\n`;
     if ($.balance > 0)
     $.message += `🧧总计红包：${$.balance}元`;
     if ($.expiredBalance > 0)
-    $.message += ` || 到期：${$.expiredBalance}元\n`;
+    $.message += ` || 到期${$.expiredBalance}元\n`;
                     } else {
                         console.log(`京东服务器返回空数据`)
                     }
